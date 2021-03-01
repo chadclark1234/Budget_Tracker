0f19e8e4318ec3166929f82c49620355db993877
Chad Clark <chadclark12@hotmail.com>
2021-03-01 09:33:01 -0600

    Mongodb and heroku

--- server.js
+++ server.js
@@ -10,14 +10,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log(`App running on port ${PORT}!`);
+    });
+  });
 
 // routes
 app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
